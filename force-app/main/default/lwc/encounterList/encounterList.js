76f77db8fefa258d989e1d8ee0759e810345abae
Mike <mbarbarelli@gmail.com>
2020-01-19 08:27:28 +0000

    Changing LWC to support display of HSM name.

--- force-app/main/default/lwc/encounterList/encounterList.js
+++ force-app/main/default/lwc/encounterList/encounterList.js
@@ -4,44 +4,31 @@ import { LightningElement, wire, track } from 'lwc';
 /** EncounterController.searchEncounters() Apex method */
 import searchEncounters from '@salesforce/apex/EncounterController.searchEncounters';
 export default class accountList extends NavigationMixin(LightningElement) {
-    
+
     @track searchTerm = '';
-    @track invalidSearchTerm = ''; 
-    validIdLength = 18;
     @wire(searchEncounters, { searchTerm: '$searchTerm' })
-    encounters;    
-    
+    encounters;
+
     handleSearchTermChange(event) {
-        // Validating searchTerm length and debounce
-        const searchTerm = event.target.value; 
-        if(searchTerm.length === this.validIdLength || searchTerm.length === 0){
-            this.invalidSearchTerm = '';
-            window.clearTimeout(this.delayTimeout);          
-            this.delayTimeout = setTimeout(() => {
-                this.searchTerm = searchTerm;
-            }, 300);
-        } else {
-            this.invalidSearchTerm = searchTerm;
-        }
+        //Debounce
+        const searchTerm = event.target.value;
+        window.clearTimeout(this.delayTimeout);
+        this.delayTimeout = setTimeout(() => {
+            this.searchTerm = searchTerm;
+        }, 300);
     }
 
-    get invalidSearchTermEntered() {
-        return (this.invalidSearchTerm.length > 0);
+    handleEncounterView(event) {
+        const encounterId = event.detail;
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: encounterId,
+                objectApiName: 'Encounter__c',
+                actionName: 'view',
+            },
+        });
     }
-
-	handleEncounterView(event) {
-		// Get bear record id from bearview event
-		const encounterId = event.detail;
-		// Navigate to bear record page
-		this[NavigationMixin.Navigate]({
-			type: 'standard__recordPage',
-			attributes: {
-				recordId: encounterId,
-				objectApiName: 'Encounter__c',
-				actionName: 'view',
-			},
-		});
-    }     
 }
 
 
